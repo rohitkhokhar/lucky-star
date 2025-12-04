f5e7389820c6105a66696a208dab244f9ada887d
VijayBapodariya <vijaybapodariya296@gmail.com>
2025-12-04 18:17:22 +0530

    addd

--- src/assets/cards/index.js
+++ src/assets/cards/index.js
@@ -40,19 +40,19 @@ import D11 from './D-11.png';
 import D12 from './D-12.png';
 import D13 from './D-13.png';
 
-import C1 from './C-1.png';
-import C2 from './C-2.png';
-import C3 from './C-3.png';
-import C4 from './C-4.png';
-import C5 from './C-5.png';
-import C6 from './C-6.png';
-import C7 from './C-7.png';
-import C8 from './C-8.png';
-import C9 from './C-9.png';
-import C10 from './C-10.png';
-import C11 from './C-11.png';
-import C12 from './C-12.png';
-import C13 from './C-13.png';
+import F1 from './F-1.png';
+import F2 from './F-2.png';
+import F3 from './F-3.png';
+import F4 from './F-4.png';
+import F5 from './F-5.png';
+import F6 from './F-6.png';
+import F7 from './F-7.png';
+import F8 from './F-8.png';
+import F9 from './F-9.png';
+import F10 from './F-10.png';
+import F11 from './F-11.png';
+import F12 from './F-12.png';
+import F13 from './F-13.png';
 
 const cardImages = {
   'S-1': S1,
@@ -97,19 +97,19 @@ const cardImages = {
   'D-12': D12,
   'D-13': D13,
 
-  'C-1': C1,
-  'C-2': C2,
-  'C-3': C3,
-  'C-4': C4,
-  'C-5': C5,
-  'C-6': C6,
-  'C-7': C7,
-  'C-8': C8,
-  'C-9': C9,
-  'C-10': C10,
-  'C-11': C11,
-  'C-12': C12,
-  'C-13': C13,
+  'F-1': F1,
+  'F-2': F2,
+  'F-3': F3,
+  'F-4': F4,
+  'F-5': F5,
+  'F-6': F6,
+  'F-7': F7,
+  'F-8': F8,
+  'F-9': F9,
+  'F-10': F10,
+  'F-11': F11,
+  'F-12': F12,
+  'F-13': F13,
 };
 
 export default cardImages;
